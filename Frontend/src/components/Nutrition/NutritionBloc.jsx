931391e770ffff1b1a568a4e836b759612930967
daiba <daiba_b@yahoo.com>
2025-01-28 18:59:05 +0100

    Formatage des Kpis et ajustements

--- Frontend/src/components/Nutrition/NutritionBloc.jsx
+++ Frontend/src/components/Nutrition/NutritionBloc.jsx
@@ -1,29 +1,72 @@
+/* eslint-disable react/prop-types */
 import "./NutritionBloc.scss";
 import caloriesIcon from "../../assets/calories.png";
 import proteinsIcon from "../../assets/proteins.png";
 import glucidesIcon from "../../assets/glucides.png";
 import lipidesIcon from "../../assets/lipides.png";
+import { getUserKpisData } from "../../Service/apiService";
+import { useEffect, useState } from "react";
 
-function NutritionBloc() {
-  const nutritionData = [
-    { icon: caloriesIcon, label: "Calories", value: "1.930Cal" },
-    { icon: proteinsIcon, label: "Proteines", value: "155g" },
-    { icon: glucidesIcon, label: "Glucides", value: "290g" },
-    { icon: lipidesIcon, label: "Lipides", value: "50g" },
-  ];
+function NutritionBloc({ id }) {
+  const [userKpis, setUserKpis] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getUserKpisData(id);
+      setUserKpis(data);
+    };
+    if (id) {
+      fetchData();
+    }
+  }, [id]);
+
+  if (!userKpis) {
+    return <p>Aucune donnée Kpis trouvée pour cet utilisateur.</p>;
+  }
+
   return (
     <div className="nutriBloc">
-      {nutritionData.map((item, index) => (
-        <div key={index} className="nutriInfo">
-          <div className="icon">
-            <img src={item.icon} alt={item.label} />
-          </div>
-          <div className="details">
-            <p className="value">{item.value}</p>
-            <p className="label">{item.label}</p>
-          </div>
+      <div className="nutriInfo">
+        <div className="icon">
+          <img src={caloriesIcon} alt="Calories" />
         </div>
-      ))}
+        <div className="details">
+          <p className="value">
+            {userKpis.calorieCount.toLocaleString("en-US")}kCal
+          </p>
+          <p className="label">Calories</p>
+        </div>
+      </div>
+
+      <div className="nutriInfo">
+        <div className="icon">
+          <img src={proteinsIcon} alt="Protéines" />
+        </div>
+        <div className="details">
+          <p className="value">{userKpis.proteinCount}g</p>
+          <p className="label">Protéines</p>
+        </div>
+      </div>
+
+      <div className="nutriInfo">
+        <div className="icon">
+          <img src={glucidesIcon} alt="Glucides" />
+        </div>
+        <div className="details">
+          <p className="value">{userKpis.carbohydrateCount}g</p>
+          <p className="label">Glucides</p>
+        </div>
+      </div>
+
+      <div className="nutriInfo">
+        <div className="icon">
+          <img src={lipidesIcon} alt="Lipides" />
+        </div>
+        <div className="details">
+          <p className="value">{userKpis.lipidCount}g</p>
+          <p className="label">Lipides</p>
+        </div>
+      </div>
     </div>
   );
 }
